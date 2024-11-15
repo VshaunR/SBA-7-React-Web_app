1f13cc34582f22e989e9f646734489131f56f0d7
VshaunR <vramshaun@gmail.com>
2024-11-15 10:10:38 -0500

    improved search to render based on category

--- src/componenets/SearchCard.jsx
+++ src/componenets/SearchCard.jsx
@@ -1,17 +1,68 @@
 
 
 
-export default function SearchCard({data}){
+export default function SearchCard({data,category}){
+
+
+  function render(category){
+
+    switch(category){
+      case "people":
+        return (<div className="card">
+          <h1>{category}</h1>
+          <h1 className="card-name">{data.name}</h1>
+          <p className="card-model"></p>
+          <p className="card-url"></p>
+          <p className="card-class"></p>
+          </div>)
+        case "starships":
+          return   (<div className="card">
+            <h1>{category}</h1>
+            <h1 className="card-name">{data.name}</h1>
+            <p className="card-model"></p>
+            <p className="card-url"></p>
+            <p className="card-class"></p>
+            </div>)
+         case "films":
+          return  (<div className="card">
+            <h1>{category}</h1>
+            <h1 className="card-name">{data.name}</h1>
+            <p className="card-model"></p>
+            <p className="card-url"></p>
+            <p className="card-class"></p>
+            </div>)
+           case "planets":
+            return  (<div className="card">
+              <h1>{category}</h1>
+              <h1 className="card-name">{data.name}</h1>
+              <p className="card-model"></p>
+              <p className="card-url"></p>
+              <p className="card-class"></p>
+              </div>)
+          case "vehicles":
+            return (<div className="card">
+              <h1>{category}</h1>
+              <h1 className="card-name">{data.name}</h1>
+              <p className="card-model"></p>
+              <p className="card-url"></p>
+              <p className="card-class"></p>
+              </div>)
+            default:
+              <h1>Loading...</h1>
+
+    }
+    
+
+  
+       
+        
+            
+  }
+
 
   return (<>
   
-  <div className="card">
-   
-    <h1 className="card-name">{data.name}</h1>
-    <p className="card-model"></p>
-    <p className="card-url"></p>
-    <p className="card-class"></p>
-    </div>
+      {render(category)}
   
   </>)
 }
