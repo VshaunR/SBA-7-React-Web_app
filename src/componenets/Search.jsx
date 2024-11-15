c238c16869df05cbe33bcc75456d56e19664b98c
VshaunR <vramshaun@gmail.com>
2024-11-15 09:11:35 -0500

    cleared search bar upon submit

--- src/componenets/Search.jsx
+++ src/componenets/Search.jsx
@@ -1,6 +1,8 @@
-import { useState,useEffect } from "react";
+import { useState } from "react";
 import SearchCard from "./SearchCard";
 export default function Search(){
+
+
 const [searchTerm,setSearchTerm] = useState("");
 const [searchCate, setSearchCate]= useState("");
 const [searchData,setSearchData]= useState([])
@@ -8,7 +10,7 @@ function handleSearchCate(e){
   setSearchCate(e.target.value)
 
 }
-console.log(searchCate)
+
   function handleChange(e){
     setSearchTerm(e.target.value)
    
@@ -16,6 +18,7 @@ console.log(searchCate)
 
  async function handleSubmit(e){
     e.preventDefault()
+     
     try {
       let arr=[]
       
@@ -27,22 +30,23 @@ console.log(searchCate)
      
         arr= arr.concat(data.results)
         setSearchData(arr)
-       
+        let str=""
+       setSearchTerm(str)
      
     } catch (e) {
         console.error(e)
     }
-    
+   
   }
-// console.log(searchData)
+
  let list = searchData.map((data)=>{
-    return <SearchCard data={data}/>
+    return <SearchCard category={searchCate} data={data}/>
 })
  
   return(<div>
 
       <form onSubmit={handleSubmit}>
-        <input type="text" name="search" id="searchTerm" onChange={handleChange}/>
+        <input type="text" name="search" id="searchTerm" value={searchTerm} onChange={handleChange}/>
         <select onChange={handleSearchCate} defaultValue={'DEFAULT'}>
           <option value="DEFAULT" disabled >Choose Category</option>
           <option value="people"  >People</option>
@@ -55,7 +59,7 @@ console.log(searchCate)
         <input type="submit" value="Search" />
       </form>
 
-         {list}
+         {list !==null ? (list):(<h1>loading</h1>)}
              
   </div>)
 }
